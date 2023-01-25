a5730e94b927a6b9d2a3c54f032bb91367d87056
Michael Kopinsky <Michael.Kopinsky@pennmedicine.upenn.edu>
2023-01-25 14:19:21 -0500

    Adjust the font-size in the emojipicker when it only has emoji

--- src/InputArea.vue
+++ src/InputArea.vue
@@ -3,11 +3,10 @@
     <textarea
       ref="textarea"
       :value="value"
-      rows="2"
       cols="30"
       class="form-control"
       :disabled="disabled"
-      @input="onInput"
+      @input="$emit('input', $event.target.value)"
     />
     <emoji-picker :search="search" @emoji="insertEmoji">
       <button
@@ -21,7 +20,7 @@
           xmlns="http://www.w3.org/2000/svg"
           class="h-6 w-6 fill-current text-grey"
         >
-          <path d="M0 0h24v24H0z" fill="none" />
+          <path d="M0 0h24v24H0z" fill="none"/>
           <path
             d="M11.99 2C6.47 2 2 6.48 2 12s4.47 10 9.99 10C17.52 22 22 17.52 22 12S17.52 2 11.99 2zM12 20c-4.42 0-8-3.58-8-8s3.58-8 8-8 8 3.58 8 8-3.58 8-8 8zm3.5-9c.83 0 1.5-.67 1.5-1.5S16.33 8 15.5 8 14 8.67 14 9.5s.67 1.5 1.5 1.5zm-7 0c.83 0 1.5-.67 1.5-1.5S9.33 8 8.5 8 7 8.67 7 9.5 7.67 11 8.5 11zm3.5 6.5c2.33 0 4.31-1.46 5.11-3.5H6.89c.8 2.04 2.78 3.5 5.11 3.5z"
           />
@@ -56,6 +55,7 @@
 </template>
 <script>
 import {EmojiPicker} from 'vue-emoji-picker'
+import emojiRegex from "emoji-regex";
 
 export default {
   name: 'InputArea',
@@ -84,6 +84,11 @@ export default {
       search: '',
     }
   },
+  watch: {
+    value() {
+      this.resizeTextarea();
+    }
+  },
   methods: {
     insertEmoji(emoji) {
       const textarea = this.$refs.textarea;
@@ -99,14 +104,12 @@ export default {
         textarea.selectionStart = textarea.selectionEnd = startPos + emoji.length
       });
     },
-    onInput(event) {
-      // debugger;
-      this.resizeTextarea(event);
-      this.$emit('input', event.target.value);
-    },
-    resizeTextarea(event) {
-      event.target.style.height = "auto";
-      event.target.style.height = `${event.target.scrollHeight}px`;
+    resizeTextarea() {
+      const textarea = this.$refs.textarea;
+      const justEmojiRegex = new RegExp(`^((${emojiRegex().source})|[ \n])+$`)
+      textarea.style.height = "auto"; // reset
+      textarea.style.fontSize = justEmojiRegex.test(textarea.value) ? '1.8rem' : null;
+      textarea.style.height = `${textarea.scrollHeight}px`;
     },
   },
 
