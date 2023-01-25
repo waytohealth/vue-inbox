332927d62d80b5cebd7f666e8c2e6e849250a52b
Michael Kopinsky <Michael.Kopinsky@pennmedicine.upenn.edu>
2023-01-25 11:09:44 -0500

    Extract the textarea into its own component

---
+++ src/InputArea.vue
@@ -0,0 +1,43 @@
+<template>
+  <textarea
+    :value="value"
+    rows="2"
+    cols="30"
+    class="form-control"
+    :disabled="disabled"
+    @input="onInput"
+  />
+</template>
+<script>
+export default {
+  name: 'InputArea',
+  props: {
+    disabled: {
+      type: Boolean,
+      default: false,
+    },
+    value: {
+      type: String,
+      required: true
+    }
+  },
+  methods: {
+    onInput(event) {
+      // debugger;
+      this.resizeTextarea(event);
+      this.$emit('input', event.target.value);
+    },
+    resizeTextarea(event) {
+      event.target.style.height = "auto";
+      event.target.style.height = `${event.target.scrollHeight}px`;
+    },
+  }
+}
+</script>
+<style scoped>
+
+textarea {
+  resize: none;
+}
+
+</style>
