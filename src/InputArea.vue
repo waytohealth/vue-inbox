9bc78cbb3dc845dc7a054118449149cc31052a4b
Michael Kopinsky <Michael.Kopinsky@pennmedicine.upenn.edu>
2023-01-26 16:19:18 -0500

    Remove deprecated slot syntax, and a small bug in textarea resizing

--- src/InputArea.vue
+++ src/InputArea.vue
@@ -9,24 +9,24 @@
       @input="$emit('input', $event.target.value)"
     />
     <emoji-picker :search="search" @emoji="insertEmoji">
-      <button
-        slot="emoji-invoker"
-        slot-scope="{ events: { click: clickEvent } }"
-        class="emoji-invoker"
-        @click.stop="clickEvent"
-      >
-        <svg
-          viewBox="0 0 24 24"
-          xmlns="http://www.w3.org/2000/svg"
-          class="h-6 w-6 fill-current text-grey"
+      <template #emoji-invoker="{ events: { click: clickEvent } }">
+        <button
+          class="emoji-invoker"
+          @click.stop="clickEvent"
         >
-          <path d="M0 0h24v24H0z" fill="none"/>
-          <path
-            d="M11.99 2C6.47 2 2 6.48 2 12s4.47 10 9.99 10C17.52 22 22 17.52 22 12S17.52 2 11.99 2zM12 20c-4.42 0-8-3.58-8-8s3.58-8 8-8 8 3.58 8 8-3.58 8-8 8zm3.5-9c.83 0 1.5-.67 1.5-1.5S16.33 8 15.5 8 14 8.67 14 9.5s.67 1.5 1.5 1.5zm-7 0c.83 0 1.5-.67 1.5-1.5S9.33 8 8.5 8 7 8.67 7 9.5 7.67 11 8.5 11zm3.5 6.5c2.33 0 4.31-1.46 5.11-3.5H6.89c.8 2.04 2.78 3.5 5.11 3.5z"
-          />
-        </svg>
-      </button>
-      <div slot="emoji-picker" slot-scope="{ emojis, insert }">
+          <svg
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+            class="h-6 w-6 fill-current text-grey"
+          >
+            <path d="M0 0h24v24H0z" fill="none" />
+            <path
+              d="M11.99 2C6.47 2 2 6.48 2 12s4.47 10 9.99 10C17.52 22 22 17.52 22 12S17.52 2 11.99 2zM12 20c-4.42 0-8-3.58-8-8s3.58-8 8-8 8 3.58 8 8-3.58 8-8 8zm3.5-9c.83 0 1.5-.67 1.5-1.5S16.33 8 15.5 8 14 8.67 14 9.5s.67 1.5 1.5 1.5zm-7 0c.83 0 1.5-.67 1.5-1.5S9.33 8 8.5 8 7 8.67 7 9.5 7.67 11 8.5 11zm3.5 6.5c2.33 0 4.31-1.46 5.11-3.5H6.89c.8 2.04 2.78 3.5 5.11 3.5z"
+            />
+          </svg>
+        </button>
+      </template>
+      <template #emoji-picker="{ emojis, insert }">
         <div class="emoji-picker">
           <div class="emoji-picker__search">
             <input
@@ -49,7 +49,7 @@
             </div>
           </div>
         </div>
-      </div>
+      </template>
     </emoji-picker>
   </div>
 </template>
@@ -85,8 +85,8 @@ export default {
     }
   },
   watch: {
-    value() {
-      this.resizeTextarea();
+    value(newVal) {
+      this.resizeTextarea(newVal);
     }
   },
   methods: {
@@ -104,11 +104,11 @@ export default {
         textarea.selectionStart = textarea.selectionEnd = startPos + emoji.length
       });
     },
-    resizeTextarea() {
+    resizeTextarea(newVal) {
       const textarea = this.$refs.textarea;
       const justEmojiRegex = new RegExp(`^((${emojiRegex().source})|[ \n])+$`)
       textarea.style.height = "auto"; // reset
-      textarea.style.fontSize = justEmojiRegex.test(textarea.value) ? '1.8rem' : null;
+      textarea.style.fontSize = justEmojiRegex.test(newVal) ? '1.8rem' : null;
       textarea.style.height = `${textarea.scrollHeight}px`;
     },
   },
