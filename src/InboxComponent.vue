f576ada91df19c6e3c0c9ed4ba5382d926141f8c
Ben Rosenbach <ben@benrosenbach.com>
2023-01-28 14:25:00 -0500

    Fix loading zone for older messages and scroll on new message to work properly for both directions of message and gallery view. We might want to switch to changing visibility instead of using v-if if we want to maintain scroll position when toggling between the two views.

--- src/InboxComponent.vue
+++ src/InboxComponent.vue
@@ -12,16 +12,13 @@
     </p>
     <div>
       <div class="inbox clearfix">
-        <div v-if="store.loading.older" class="text-center">
-          <b-spinner variant="primary" label="Spinning" />
-        </div>
-
         <GalleryView
           v-if="galleryView"
           :messages-with-images="messagesWithImages"
           :store="store"
           :inbox-helper="inboxHelper"
           @openImageLightbox="openImageLightbox"
+          :show-load-more="store.loading.older"
         />
         <MessageView
           v-else
@@ -29,6 +26,7 @@
           :inbox-helper="inboxHelper"
           :store="store"
           @openImageLightbox="openImageLightbox"
+          :show-load-more="store.loading.older"
         />
       </div>
     </div>
@@ -153,9 +151,17 @@ export default {
   watch: {
     latestMessageId() {
       this.$nextTick(() => {
-        this.scrollToBottom();
+        this.scrollToNewest();
       });
     },
+    galleryView(newVal) {
+      // scroll to newest when changing back to message view
+      if (!newVal) {
+        this.$nextTick(() => {
+          this.scrollToNewest();
+        });
+      }
+    }
   },
   async created() {
     this.store.apiBaseUrl = this.apiBaseUrl;
@@ -167,7 +173,7 @@ export default {
     this.store.loadMessages()
       .then(() => {
         this.loading = false;
-        this.scrollToBottom();
+        this.scrollToNewest();
       }).catch((e) => {
       // TODO: proper error handling
       console.log(e);
@@ -178,10 +184,10 @@ export default {
     // Only scroll on first mount - if we e.g. switch back and forth between tabs, on subsequent mounts
     // it should stay where we were
     if (!this.scrolled) {
-      this.scrollToBottom();
+      this.scrollToNewest();
       this.scrolled = true;
     }
-    this.handleTop();
+    this.handleLoadMore();
     this.poll();
   },
   methods: {
@@ -189,21 +195,28 @@ export default {
       this.showImageLightbox = true;
       this.$refs.imageLightbox.open(msgId, imageIndex, this.galleryView);
     },
-    scrollToBottom() {
+    scrollToNewest() {
       const el = this.$el.querySelector('.inbox');
+      const scrollToOffset = this.galleryView ? 0 : el.scrollHeight;
       if (el.scrollTo) {
-        el.scrollTo({top: el.scrollHeight, behavior: 'smooth'});
+        el.scrollTo({top: scrollToOffset, behavior: 'smooth'});
       } else {
         // IE fallback
-        el.scrollTop = el.scrollHeight;
+        el.scrollTop = scrollToOffset;
       }
     },
-    handleTop() {
+    handleLoadMore() {
       let inboxDiv = this.$el?.querySelector('.inbox');
       inboxDiv.onscroll = () => {
         if (inboxDiv) {
-          if (inboxDiv.scrollTop === 0) {
-            store.loadOlder();
+          if (this.galleryView) {
+            if (inboxDiv.scrollTop >= inboxDiv.scrollHeight - inboxDiv.clientHeight - 1) {
+              store.loadOlder();
+            }
+          } else {
+            if (inboxDiv.scrollTop === 0) {
+              store.loadOlder();
+            }
           }
         }
       }
@@ -211,7 +224,7 @@ export default {
     async sendMessage() {
       if (this.textContent.length) {
         await this.store.sendMessage(this.textContent);
-        this.scrollToBottom();
+        this.scrollToNewest();
         this.textContent = "";
       }
     },
