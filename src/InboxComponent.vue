3eb0cd0acbd2c509d2a1fbc070c1d0f5ec51a90d
Michael Kopinsky <Michael.Kopinsky@pennmedicine.upenn.edu>
2023-02-02 09:31:00 -0500

    Fix sending of images
    This got accidentally messed up in ec14dbc308b5dde7afb4804b4264fa78a5e54878

--- src/InboxComponent.vue
+++ src/InboxComponent.vue
@@ -260,8 +260,8 @@ export default {
       }
     },
     async sendMessage() {
-      if (this.textContent.length) {
-        await this.store.sendMessage(this.textContent);
+      if (this.textContent.length || this.imageUrl) {
+        await this.store.sendMessage(this.textContent, this.imageUrl);
         this.scrollToNewest();
         this.textContent = "";
         this.imageUrl = '';
